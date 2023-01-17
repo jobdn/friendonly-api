dd7a60cb042b019e539ba11ca78e4ed0922642e1
Pisarev Danila <pisarev@softlab.ru>
2023-01-17 18:31:58 +0300

    feat: add vercel and cors setup

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,13 @@
+const whitelist = [process.env.CLIENT_URL, process.env.LOCAL_CLIENT];
+
 module.exports = {
-  origin: [process.env.CLIENT_URL],
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
